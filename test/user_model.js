df16e41aecde53e34e71b8ee277b55eab4a693ad
José Airosa <me@joseairosa.com>
2017-03-11 23:59:53 +0000

    Fixed typo

--- test/user_model.js
+++ test/user_model.js
@@ -5,7 +5,7 @@ module.exports = function(sequelize, models) {
 
     describe('User Model', function() {
         it('should create a model with valid properties', function(done) {
-            models.Article.create({
+            models.User.create({
                     first_name: 'Squall',
                     last_name: 'Lionheart',
                     email: 'squall@finalfantasy.viii'
