f7d7455e436199b2964c836ca15c0a9374666c39
José Airosa <me@joseairosa.com>
2017-03-11 14:15:52 +0000

    Setting up postgres config

---
+++ config/postgres.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    database: process.env.FLOCK_USER_MANAGEMENT_DEVELOPMENT_DATABASE,
+    username: process.env.FLOCK_USER_MANAGEMENT_DEVELOPMENT_USERNAME,
+    password: process.env.FLOCK_USER_MANAGEMENT_DEVELOPMENT_PASSWORD,
+    host: process.env.FLOCK_USER_MANAGEMENT_DEVELOPMENT_HOST,
+    dialect: 'postgres'
+  },
+  test: {
+    database: process.env.FLOCK_USER_MANAGEMENT_TEST_DATABASE,
+    username: process.env.FLOCK_USER_MANAGEMENT_TEST_USERNAME,
+    password: process.env.FLOCK_USER_MANAGEMENT_TEST_PASSWORD,
+    host: process.env.FLOCK_USER_MANAGEMENT_TEST_HOST,
+    dialect: 'postgres'
+  },
+  production: {
+    database: process.env.FLOCK_USER_MANAGEMENT_PRODUCTION_DATABASE,
+    username: process.env.FLOCK_USER_MANAGEMENT_PRODUCTION_USERNAME,
+    password: process.env.FLOCK_USER_MANAGEMENT_PRODUCTION_PASSWORD,
+    host: process.env.FLOCK_USER_MANAGEMENT_PRODUCTION_HOST,
+    dialect: 'postgres'
+  }
+};
