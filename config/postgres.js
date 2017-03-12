0ad49f4a1a185a45c8f22dbe003ca3a59e579ea5
José Airosa <me@joseairosa.com>
2017-03-12 00:21:13 +0000

    Defining logging for postgres

--- config/postgres.js
+++ config/postgres.js
@@ -4,20 +4,23 @@ module.exports = {
     username: process.env.FLOCK_USER_MANAGEMENT_DEVELOPMENT_USERNAME,
     password: process.env.FLOCK_USER_MANAGEMENT_DEVELOPMENT_PASSWORD,
     host: process.env.FLOCK_USER_MANAGEMENT_DEVELOPMENT_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    logging: true
   },
   test: {
     database: process.env.FLOCK_USER_MANAGEMENT_TEST_DATABASE,
     username: process.env.FLOCK_USER_MANAGEMENT_TEST_USERNAME,
     password: process.env.FLOCK_USER_MANAGEMENT_TEST_PASSWORD,
     host: process.env.FLOCK_USER_MANAGEMENT_TEST_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    logging: false
   },
   production: {
     database: process.env.FLOCK_USER_MANAGEMENT_PRODUCTION_DATABASE,
     username: process.env.FLOCK_USER_MANAGEMENT_PRODUCTION_USERNAME,
     password: process.env.FLOCK_USER_MANAGEMENT_PRODUCTION_PASSWORD,
     host: process.env.FLOCK_USER_MANAGEMENT_PRODUCTION_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    logging: false
   }
 };
