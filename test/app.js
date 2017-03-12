634d85946b034ec157d4ae10c6578b9f0909b37d
José Airosa <me@joseairosa.com>
2017-03-12 15:14:40 +0000

    Added behaviour tests around user delete endpoint

--- test/app.js
+++ test/app.js
@@ -61,19 +61,66 @@ describe('API', function() {
                     last_name: 'Lionheart',
                     email: 'squall@finalfantasy.viii'
                 })
+                .end(function() {
+                    request(server)
+                        .put('/users/1')
+                        .send({
+                            first_name: 'Rinoa',
+                            last_name: 'Heartilly',
+                            email: 'rinoa@finalfantasy.viii'
+                        })
+                        .expect(200)
+                        .expect({
+                            "id": 1,
+                            "first_name": "Rinoa",
+                            "last_name": "Heartilly",
+                            "email": "rinoa@finalfantasy.viii"
+                        })
+                        .end(function(err, res) {
+                            if (err) return done(err);
+                            done();
+                        });
+                });
+        });
+    });
+
+    describe('DELETE /users/:id', function() {
+        it('responds with a 200 OK when user deleted', function testSlash(done) {
             request(server)
-                .put('/users/1')
+                .post('/users')
                 .send({
-                    first_name: 'Rinoa',
-                    last_name: 'Heartilly',
-                    email: 'rinoa@finalfantasy.viii'
+                    first_name: 'Squall',
+                    last_name: 'Lionheart',
+                    email: 'squall@finalfantasy.viii'
                 })
-                .expect(200)
+                .end(function() {
+                    request(server)
+                        .delete('/users/1')
+                        .expect(200)
+                        .expect({
+                            "deleted": true
+                        })
+                        .end(function(err, res) {
+                            if (err) return done(err);
+                            done();
+                        });
+                });
+        });
+
+        it('responds with a 500 ERROR when user cannot be found for deletion', function testSlash(done) {
+            request(server)
+                .post('/users')
+                .send({
+                    first_name: 'Squall',
+                    last_name: 'Lionheart',
+                    email: 'squall@finalfantasy.viii'
+                })
+            request(server)
+                .delete('/users/999')
+                .expect(500)
                 .expect({
-                    "id": 1,
-                    "first_name": "Rinoa",
-                    "last_name": "Heartilly",
-                    "email": "rinoa@finalfantasy.viii"
+                    "error": {},
+                    "message": 'Cannot find user ID 999'
                 })
                 .end(function(err, res) {
                     if (err) return done(err);
